4a35dda2f3929a602486d7e096ac32d663295187
kamilov2 <jasurkamilov7279@gmail.com>
2024-05-30 12:19:24 +0500

    .

--- 3-oy javascript/2-dars data types and variables/main.js
+++ 3-oy javascript/2-dars data types and variables/main.js
@@ -1,5 +1,21 @@
-var ism = 'Alisher'
-let email = 'main@gmail.com'
-const married = true
-var ketmon = '123'
-console.log(ketmon)
+let data_client = {  
+    client_1:[{
+        name:"Alex",
+        data:[
+            {
+                phone_number:900000000                
+            },
+            {
+                age:15
+            }
+        ]
+    },
+    {
+        name:"Jake"
+    },
+    {
+        name:"Jordan"
+    }
+]    
+}
+console.log(data_client.client_1[0].data[1].age)
