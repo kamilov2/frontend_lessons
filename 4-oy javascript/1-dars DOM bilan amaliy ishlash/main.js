a8c7ba413041d4a1d80d75f69d8306e54374edb6
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-04 11:27:55 +0500

    .

--- 4-oy javascript/1-dars DOM bilan amaliy ishlash/main.js
+++ 4-oy javascript/1-dars DOM bilan amaliy ishlash/main.js
@@ -6,17 +6,32 @@
 
 // element.setAttribute('class', 'false')
 
-const element = document.getElementById('myId')
-const d = element.textContent
-if(d === "null"){
-    element.setAttribute('class' , 'null')
-    element.innerText = "Davomat olinmagan"
-}else if(d === "false"){
-    element.setAttribute('class', 'false')
-    element.innerText = "Kelmadi"
-}else if(d === "true"){
-    element.setAttribute('class', 'true')
-    element.innerText = "Keldi"
-}else{
-    element.innerText = "Backendan kelgan malumot notogri!"
-}
+// const element = document.getElementById('myId')
+// const d = element.textContent
+// if(d === "null"){
+//     element.setAttribute('class' , 'null')
+//     element.innerText = "Davomat olinmagan"
+// }else if(d === "false"){
+//     element.setAttribute('class', 'false')
+//     element.innerText = "Kelmadi"
+// }else if(d === "true"){
+//     element.setAttribute('class', 'true')
+//     element.innerText = "Keldi"
+// }else{
+//     element.innerText = "Backendan kelgan malumot notogri!"
+// }
+
+// function changeBackgroundColor() {
+//     const divs = document.querySelectorAll('div');
+//     divs.forEach(div => {
+//       div.setAttribute('style', 'background-color: black;');
+//     });
+//   }
+// const element = document.getElementById("myId").getAttribute("")
+// let a = element.getAttribute("class")
+// console.log(element.textContent)
+// console.log(a)
+
+const element = document.getElementById("myId")
+const f = element.removeAttribute('class')
+
