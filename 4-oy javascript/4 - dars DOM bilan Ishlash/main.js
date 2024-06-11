df7bf517a10cfd267dac4ba26cc94fc8892d9efd
kamilov2 <jasurkamilov7279@gmail.com>
2024-06-11 11:31:28 +0500

    .

---
+++ 4-oy javascript/4 - dars DOM bilan Ishlash/main.js
@@ -0,0 +1,21 @@
+ // const d = document.getElementById('myId')
+// d.style.color = 'black'
+// d.style.backgroundColor = 'yellow'      
+
+// const element = document.getElementById('myId')
+// element.classList.add('myClass2')
+
+
+
+// const element = document.getElementById('myId')
+// element.classList.remove('myClass3')
+
+// const element = document.getElementById('myId')
+// element.classList.toggle("myClass2")
+
+
+// const element = document.getElementById('myId')
+// let s = element.classList.contains('myClass2')
+// if(s === true){
+//     console.log('hello')
+// }
